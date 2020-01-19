a084920c5801f91c6f1a03d8cec5832598cf8c87
Mahmoud Nafea <mahmoudnafea19@gmail.com>
2020-01-19 12:34:45 +0800

    deploy to firebase

---
+++ build/precache-manifest.d1c215b70bdbdc4831ba0338ded128ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd310cba598251cd250f128cb7cbbe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "468a6efdd673705aba5f",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "09dd289260162be083e0",
+    "url": "/static/css/main.aa432f85.chunk.css"
+  },
+  {
+    "revision": "468a6efdd673705aba5f",
+    "url": "/static/js/2.a52526c3.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/static/js/2.a52526c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09dd289260162be083e0",
+    "url": "/static/js/main.a6baf8d1.chunk.js"
+  },
+  {
+    "revision": "ddb348687ead292861da",
+    "url": "/static/js/runtime-main.92683889.js"
+  }
+]);
